22db192304004aac06adf61406b74063ab9bf501
Rohan Rudra <rohanrudra55@gmail.com>
2022-08-31 00:55:34 +0530

    Initial

---
+++ src/testant/TestANT.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testant;
+public class TestANT {
+    public static void main(String[] args) {
+        System.out.println("Hello world! From CLI");
+    }
+    
+}
---
+++ src/testant/testDisp.java
@@ -0,0 +1,16 @@
+package testant;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author alpha
+ */
+public class testDisp {
+    public String show(String n){
+        return "Hello world "+n+" !";
+    }
+}
